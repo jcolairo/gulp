d950227858405c934450adb38c8d8966329d04ab
James Colairo <jamescolairo37@gmail.com>
2017-10-16 18:24:15 +0100

    added sass watch to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 // require modules
-
 var gulp   = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
 
 // first task
 // Scripts task
@@ -13,10 +13,18 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('app/js'));
 });
 
+// compass / sass task
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(uglify())
+    .pipe(gulp.dest('app/css'));
+});
+
 // watch task
 gulp.task('watch', function() {
-  gulp.watch('app/js/**/*.js', ['scripts']);
+  gulp.watch('app/js/**/*.js', ['scripts', 'sass']);
 });
 
 // default task
-gulp.task('default', ['scripts', 'watch']);
+gulp.task('default', ['scripts', 'watch', 'sass']);
